f8f1ad4c62426c3768540300d2640124d6d3bd51
abiliobitt <abilio.bjr@gmail.com>
2023-05-26 11:07:15 -0300

    feat(home): create pages base and home component

---
+++ src/presentation/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from '../../components'
+
+const Home: React.FC = () => {
+    return (
+        <>
+            <NavBar />
+            <h1>Home</h1>
+        </>
+    )
+}
+
+export default Home
